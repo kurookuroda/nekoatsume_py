595a9870e37d79bac5b888868dfa6c6d978d6fc4
kurookuroda <kurookuroda@gmail.com>
2026-09-20 04:41:08 +0900

    Update yard.py

--- nekoatsume/lib/yard.py
+++ nekoatsume/lib/yard.py
@@ -12,28 +12,84 @@ except NameError:
     pass
 
 
+def get_choice(options, prompt, prefix):
+    """
+    番号または名前で選択を受け付ける共通関数。
+    
+    options: [(key, display_name), ...] のリスト
+    戻り値: 選ばれた key、または None（キャンセル/無効）
+    """
+    if len(options) == 0:
+        printer.warn(prefix, "No options available!")
+        return None
+
+    # 番号付きで表示
+    for i, (key, name) in enumerate(options, 1):
+        printer.yard(prefix, "{0}. {1}".format(i, name))
+    
+    raw = input("{.YARD}{}{.ENDC} {} ".format(
+        printer.PColors, prefix, printer.PColors, prompt))
+    
+    # ENTERだけならキャンセル
+    if raw.strip() == "":
+        return None
+    
+    # 番号入力かどうか
+    try:
+        idx = int(raw.strip())
+        if 1 <= idx <= len(options):
+            return options[idx - 1][0]
+    except ValueError:
+        pass
+    
+    # 文字入力（大文字小文字・冠詞無視でマッチ）
+    raw_norm = raw.strip().lower()
+    raw_norm = raw_norm.lstrip("a ").lstrip("an ").lstrip("the ")
+    
+    for key, name in options:
+        if raw_norm == key.lower():
+            return key
+        # 表示名でもマッチ
+        name_norm = name.lower().lstrip("a ").lstrip("an ").lstrip("the ")
+        if raw_norm == name_norm:
+            return key
+    
+    printer.warn(prefix, "I'm sorry I didn't recognize that option")
+    return None
+
+
 def menu(data):
     """Display yard menu."""
-    data["prefix"] = "[The Yard]".format(printer.PColors, printer.PColors)
+    data["prefix"] = "[The Yard]"
     list_yard_items(data)
     data["in_yard"] = True
-    #TODO add a picture taking option
-    actions = {"list owned items": list_owned_items,
-               "examine yard": list_yard_items,
-               "cats": cats,
-               "place toy": place,
-               "place food": food,
-               "leave yard": exit}
+    
+    actions = [
+        ("list owned items", "List owned items"),
+        ("examine yard", "Examine yard"),
+        ("cats", "Look at cats"),
+        ("place toy", "Place toy"),
+        ("place food", "Place food"),
+        ("leave yard", "Leave yard"),
+    ]
+    
     while data["in_yard"]:
-        data["completer"].set_actions(actions.keys())
-        printer.prompt(data["prefix"], actions.keys())
-        inp = input("{.YARD}{}{.ENDC} What do you want to do? ".format(
-            printer.PColors, data["prefix"], printer.PColors))
-        if inp in actions:
-            actions[inp](data)
+        choice = get_choice(actions, "What do you want to do?", data["prefix"])
+        
+        if choice is None:
             continue
-        else:
-            printer.invalid(data["prefix"])
+        elif choice == "leave yard":
+            exit(data)
+        elif choice == "list owned items":
+            list_owned_items(data)
+        elif choice == "examine yard":
+            list_yard_items(data)
+        elif choice == "cats":
+            cats(data)
+        elif choice == "place toy":
+            place(data)
+        elif choice == "place food":
+            food(data)
 
 
 def compute_space(data):
@@ -65,7 +121,6 @@ def list_yard_items(data):
                 cats = ", and ".join([cat for cat in item["occupant"]])
             printer.yard(data["prefix"], "Your yard currently has a {0} in it, occupied by {1}".format(item["name"], cats))
         cat_activities(data)
-    # TODO: add cat descriptions
     else:
         printer.warn(data["prefix"], "You currently have nothing in your yard, how sad")
     check_food(data)
@@ -79,21 +134,22 @@ def cat_activities(data):
         for cat in cats:
             printer.yard(data["prefix"], "{0} is playing with a {1}".format(cat, item[0]['name']))
 
+
 def cats(data):
     """Allow you to look at the cats in your yard"""
     cats_in_yard = []
     [cats_in_yard.extend(obj["occupant"]) for obj in data["yard"] if obj["occupied"]]
-    cat_names = [cat for cat in cats_in_yard]
-    data["completer"].set_actions(cat_names)
-    printer.yard(data["prefix"], "These are the cats in your yard:")
-    for cat in cat_names:
-        printer.yard(data["prefix"],cat)
-    inp = input("{.YARD}{}{.ENDC} Which cat would you like to look at? ".format(
-        printer.PColors, data["prefix"], printer.PColors))
-    if inp in cat_names:
-        desc_cat(data, [cat for cat in data["cats"] if cat == inp][0])
-    else:
-        printer.warn(data["prefix"], "I'm sorry that cat isn't in your yard!")
+    
+    if len(cats_in_yard) == 0:
+        printer.warn(data["prefix"], "There are no cats in your yard right now!")
+        return
+    
+    cat_options = [(cat, cat) for cat in cat_names]
+    choice = get_choice(cat_options, "Which cat would you like to look at?", data["prefix"])
+    
+    if choice:
+        desc_cat(data, choice)
+
 
 def desc_cat(data, cat):
     printer.yard(data["prefix"], data["cats"][cat]["desc"])
@@ -103,20 +159,21 @@ def place(data):
     """Place item in yard."""
     items_list = [item for item in data["items"].values()
                   if "owned" in item["attributes"] and not item["in_yard"]]
-    # TODO: this is ultra gross
-    placable_items = {}
+    
+    placable_items = []
     for item in items_list:
-        #deliniate placable items from things like food
         if item["size"] < 15:
-            placable_items[item["name"]] = item
-    data["completer"].set_actions(placable_items.keys())
-    printer.yard(data["prefix"], "Here are the items that you can put in your yard: {0}".format(", ".join(placable_items.keys())))
-    inp = input("{.YARD}{}{.ENDC} Which item would you like to place? ".format(
-        printer.PColors, data["prefix"], printer.PColors))
-    if inp in placable_items.keys():
-        try_to_place(data, placable_items[inp])
-    else:
-        printer.warn(data["prefix"], "I'm sorry I didn't recognize that item")
+            placable_items.append((item["name"], item["name"]))
+    
+    if len(placable_items) == 0:
+        printer.warn(data["prefix"], "You don't have any items to place!")
+        return
+    
+    choice = get_choice(placable_items, "Which item would you like to place?", data["prefix"])
+    
+    if choice:
+        item = next(item for item in items_list if item["name"] == choice)
+        try_to_place(data, item)
 
 
 def try_to_place(data, item):
@@ -126,29 +183,25 @@ def try_to_place(data, item):
         item["in_yard"] = True
         printer.success(data["prefix"], "Nice! Your yard now consists of a {0}".format(", and a ".join([toy["name"] for toy in data["yard"]])))
     else:
-        printer.warn(data["prefix"], "Oops that won't fit in your yard! Would you like to remove an item?")
+        printer.warn(data["prefix"], "Oops that won't fit in your yard! Would you like to replace an item?")
         offer_replace(data, item)
 
 
 def offer_replace(data, item):
     """Replace an existing item in yard."""
-    yard_items = [toy["name"] for toy in data["yard"]]
-    printer.yard(data["prefix"], "Currently in your yard you have: a {0}".format(", and a".join(yard_items)))
-    data["completer"].set_actions(yard_items)
-    inp = input("{.YARD}{}{.ENDC} Would you like to replace any of the items in your yard? Which one? ".format(printer.PColors, data["prefix"], printer.PColors))
-    if inp in yard_items:
-        remove_from_yard(data, inp)
+    yard_items = [(toy["name"], toy["name"]) for toy in data["yard"]]
+    
+    choice = get_choice(yard_items, "Which item would you like to replace?", data["prefix"])
+    
+    if choice:
+        remove_from_yard(data, choice)
         try_to_place(data, item)
-    else:
-        printer.warn(data["prefix"], "Sorry I don't see that item in your yard")
 
 
 def remove_from_yard(data, item_name):
     """Remove item from yard."""
-    # TODO: god help me
     to_remove = [item for item in data["yard"] if item["name"] == item_name]
     for item in to_remove:
-        # TODO: clear cats from this
         data["yard"].remove(item)
         item["in_yard"] = False
 
@@ -164,19 +217,22 @@ def check_food(data):
 def food(data):
     """Display food placement options."""
     check_food(data)
-    placable_items = {}
+    
+    # 所有餌を集計（重複も個別に表示）
+    placable_items = []
     for idx, item in enumerate(data["owned_food"]):
-        if item["name"] not in placable_items.keys():
-            placable_items[item["name"]] = [1, idx]
-        else:
-            placable_items[item["name"]][0] += 1
-    printer.yard(data["prefix"], "Here's what you can place:")
-    for key in placable_items.keys():
-        printer.yard(data["prefix"], "A {0} ({1})".format(key, placable_items[key][0]))
-    data["completer"].set_actions(placable_items.keys())
-    to_place = input("{.YARD}{}{.ENDC} Which would you like to place? (hit ENTER if none) ".format(printer.PColors, data["prefix"], printer.PColors))
-    if to_place in placable_items.keys():
-        put_food_in_yard(data, placable_items[to_place][1])
+        # keyにインデックスも含めて一意にする
+        placable_items.append((str(idx), "{0} ({1})".format(item["name"], idx)))
+    
+    if len(placable_items) == 0:
+        printer.warn(data["prefix"], "You don't have any food! Buy some in the shop first!")
+        return
+    
+    choice = get_choice(placable_items, "Which would you like to place? (number or ENTER to cancel)", data["prefix"])
+    
+    if choice:
+        idx = int(choice)
+        put_food_in_yard(data, idx)
 
 
 def put_food_in_yard(data, arr_idx):
